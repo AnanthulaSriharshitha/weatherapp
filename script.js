9795222b293b91cef02a626d16fb27ff3e72597f
AnanthulaSriharshitha <sriharshithaananthula09@gmail.com>
2025-10-28 22:29:47 +0530

    Add files via upload
    updated project files

--- script.js
+++ script.js
@@ -1,59 +1,93 @@
+const weather = {
+  apiKey: "253047a8fb71256ad2e69d4b85300d50",
+  
+  fetchWeather: function(city) {
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${this.apiKey}')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("City not found");
+        }
+        return response.json();
+      })
+      .then((data) => this.displayWeather(data))
+      .catch((error) => this.showError(error));
+  },
+  
+  displayWeather: function(data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity, feels_like, pressure } = data.main;
+    const { speed } = data.wind;
+    const { sunrise, sunset } = data.sys;
 
+    document.querySelector(".city").innerText = "Weather in " + name;
+    document.querySelector(".temp").innerText = temp + "°C";
+    document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+    document.querySelector(".wind").innerText = "Wind speed: " + speed + " km/hr";
+    document.querySelector(".feels-like").innerText = "Feels like: " + feels_like + "°C";
+    document.querySelector(".pressure").innerText = "Pressure: " + pressure + " hPa";
+    document.querySelector(".sunrise").innerText = "Sunrise: " + new Date(sunrise * 1000).toLocaleTimeString();
+    document.querySelector(".sunset").innerText = "Sunset: " + new Date(sunset * 1000).toLocaleTimeString();
+    document.querySelector(".error").classList.add("hidden");
 
-const apiKey = "e18e4afea3f13859839d69eb9578f4d7";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+    this.setBackgroundImage(name);
+  },
 
-const searchBox = document.querySelector(".search input");
-const searchBtn = document.querySelector(".search button");
-const weatherIcon = document.querySelector(".weatherIcon");
+  setBackgroundImage: function(city) {
+    const unsplashApiKey = 'NMJYnSNiekY2WnQuk1B-OlOP4EDsBWj53rg5axg4opg';
+    const unsplashApiUrl = 'https://api.unsplash.com/search/photos?query=${encodeURIComponent(city)}&client_id=${unsplashApiKey}';
 
-async function checkWeather(city) {
-    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    fetch(unsplashApiUrl)
+      .then(response => response.json())
+      .then(data => {
+        if (data.results.length > 0) {
+          const imageUrl = data.results[0].urls.regular;
+          document.body.style.backgroundImage = url('${imageUrl}');
+        } else {
+          console.error('No images found for the location.');
+          document.body.style.backgroundImage = '';
+        }
+      })
+      .catch(error => {
+        console.error('Error fetching background image:', error);
+      });
+  },
 
-    if (response.status == 404) {
-        document.querySelector(".error").style.display = "block";
-        document.querySelector(".weather").style.display = "none";
+  showError: function(error) {
+    document.querySelector(".error").classList.remove("hidden");
+  },
+
+  search: function() {
+    const city = document.querySelector(".search-bar").value;
+    if (city) {
+      this.fetchWeather(city);
     }
+  },
 
-    else {
+  fetchDefaultWeather: function() {
+    const city = localStorage.getItem("lastCity") || "Jodhpur";
+    this.fetchWeather(city);
+  },
 
-        var data = await response.json();
+  init: function() {
+    document.querySelector(".search button").addEventListener("click", () => {
+      const city = document.querySelector(".search-bar").value;
+      this.search();
+      localStorage.setItem("lastCity", city);
+    });
 
+    document.querySelector(".search-bar").addEventListener("keyup", (event) => {
+      if (event.key === "Enter") {
+        const city = event.target.value;
+        this.search();
+        localStorage.setItem("lastCity", city);
+      }
+    });
 
-        document.querySelector(".city").innerHTML = data.name;
-        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
-        document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
-        document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+    this.fetchDefaultWeather();
+  }
+};
 
-        if (data.weather[0].main == "Clouds") {
-            weatherIcon.src = "images/clouds.png";
-        }
-
-        else if (data.weather[0].main == "Clear") {
-            weatherIcon.src = "images/clear.png";
-        }
-
-        else if (data.weather[0].main == "Rain") {
-            weatherIcon.src = "images/rain.png";
-        }
-
-        else if (data.weather[0].main == "Drizzle") {
-            weatherIcon.src = "images/drizzle.png";
-        }
-
-        else if (data.weather[0].main == "Mist") {
-            weatherIcon.src = "images/mist.png";
-        }
-
-        document.querySelector(".weather").style.display = "block";
-        document.querySelector(".error").style.display = "none";
-    }
-
-}
-
-
-searchBtn.addEventListener("click", () => {
-
-    checkWeather(searchBox.value);
-
-})
+weather.init();
